8099464a53982564c0e7f1b77a89e9b3c8617917
Victor Stupitsyn <st.viktorr@gmail.com>
2023-02-17 19:20:43 +0300

    добавил карту

--- source/js/script.js
+++ source/js/script.js
@@ -68,3 +68,32 @@ inputs.forEach((el, index) => {
     setRangeSlider(index, e.currentTarget.value);
   })
 })
+
+
+/* Map */
+
+const position = [59.968266, 30.3174089];
+const map = L.map('map').setView(position, 17);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+
+const mainPinIcon = L.icon({
+  iconUrl: '../img/Map-Pin.svg',
+  iconSize: [39, 50],
+  iconAnchor: [18, 50],
+});
+
+const mainPin = L.marker(
+  {
+    lat: position[0],
+    lng: position[1],
+  },
+  {
+    draggable: false,
+    icon: mainPinIcon,
+  },
+);
+mainPin.addTo(map);
