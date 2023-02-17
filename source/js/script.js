2eee5f44e4d36cbf68fab716e60b3dc4e1926e36
Victor Stupitsyn <st.viktorr@gmail.com>
2023-02-17 16:22:51 +0300

    filter 1.0

--- source/js/script.js
+++ source/js/script.js
@@ -17,7 +17,7 @@ navToggle.addEventListener('click', function() {
 });
 
 
-/* Slider */
+/* Gallery */
 
 const swiper = new Swiper('.swiper', {
   loop: true,
@@ -33,3 +33,38 @@ const swiper = new Swiper('.swiper', {
   },
 
 });
+
+
+//range input
+
+const slider = document.getElementById('slider');
+
+noUiSlider.create(slider, {
+    start: [400, 800],
+    connect: true,
+    range: {
+        'min': 0,
+        'max': 900
+    }
+});
+
+const input0 = document.getElementById('input-0');
+const input1 = document.getElementById('input-1');
+const inputs = [input0, input1]
+
+
+slider.noUiSlider.on('update', function (values, handle) {
+  inputs[handle].value = Math.round(values[handle])
+})
+
+const setRangeSlider = (i, value) => {
+  let arr = [null, null];
+  arr[i] = value;
+  slider.noUiSlider.set(arr)
+};
+
+inputs.forEach((el, index) => {
+  el.addEventListener('change', e => {
+    setRangeSlider(index, e.currentTarget.value);
+  })
+})
